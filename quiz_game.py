ee835cefab71eb8d563ee4f5364acd5e7912aadb
Rahul <155900250+DeveloperRahulSt@users.noreply.github.com>
2024-05-30 02:31:26 +0530

    my projects code

---
+++ quiz_game.py
@@ -0,0 +1,30 @@
+print("Welcome to my math quiz!")
+
+ifPlaying = input("Do you want to play? ")
+
+if ifPlaying.lower() != "yes":
+    quit()
+
+print("Let's begin!")
+
+score = 0
+
+quiz = input("How much is 12-5? ")
+if quiz == "7":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+quiz = input("How much is 5*8? ")
+if quiz == "40":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " answers correct.")
+
+
+
+
